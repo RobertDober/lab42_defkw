7f15660865dfa8928f3f46d7920ef6c5d0089687
RobertDober <robert.dober@gmail.com>
2015-12-06 12:48:11 +0100

    version dumped to 0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Kwfuns.Mixfile do
 
   def project do
     [app: :kwfuns,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.1",
      description: description,
      package: package,
