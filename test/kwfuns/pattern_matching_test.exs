b04e9d1914f63d39be387c3c68b90cf9a0ebbf36
RobertDober <robert.dober@gmail.com>
2015-12-18 18:53:36 +0100

    wipped tests for v0.1

--- test/kwfuns/pattern_matching_test.exs
+++ test/kwfuns/pattern_matching_test.exs
@@ -3,15 +3,20 @@ defmodule Kwfuns.PatternMatchingTest do
   use ExUnit.Case
   use Kwfuns
 
-  # # For 0.1
-  # defkw fun(true, x), do: x 
-  # defkw fun(_, x), do: :error
-  
-  # test "true case" do
-  #   assert fun(true, 42) == 42
-  # end
 
-  # test "default case" do
-  #   assert fun(42, 42) == :error
-  # end
+  @moduletag :wip
+  setup do
+    defmodule F do
+      defkw fun(true, x), do: x 
+      defkw fun(_, x), do: :error
+    end
+  end
+
+  test "true case" do
+    assert F.fun(true, 42) == 42
+  end
+
+  test "default case" do
+    assert F.fun(42, 42) == :error
+  end
 end
--- test/kwfuns/positionals_with_defaults_test.exs
+++ test/kwfuns/positionals_with_defaults_test.exs
@@ -3,10 +3,16 @@ defmodule Kwfuns.PositionalsWithDefaultsTest do
   use ExUnit.Case
   use Kwfuns
 
+  @moduletag :wip
+
   # For 0.1
-  # defkw fun(pos \\ :pos, kw: :kw), do: {pos, kw}
+  setup do
+    defmodule D do
+       defkw fun(pos \\ :pos, kw: :kw), do: {pos, kw}
+    end
+  end
   
-  # test "all defaults" do
-  #   assert fun() == {:pos, :kw}
-  # end
+  test "all defaults" do
+    assert D.fun() == {:pos, :kw}
+  end
 end
